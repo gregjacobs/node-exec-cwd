9e50a7b16efdc1b71654bb7dfc347b1662ad986a
Gregory Jacobs <gregory.jacobs@credit-suisse.com>
2014-06-03 19:04:13 -0400

    Add environment variables output before and after the first `cd`

--- test.js
+++ test.js
@@ -1,12 +1,19 @@
 var exec = require( 'child_process' ).exec;
 
-console.log( "\nprocess.cwd()\t\t: " + process.cwd()  );
+console.log( "\nprocess.cwd()\t\t: " + process.cwd() );
 
-exec( 'cd', function( error, stdout ) {
-	console.log( "`cd` stdout\t\t: " + stdout );
+exec( 'set > envBefore.txt', doFirstCd );
+
+function doFirstCd() {
+	exec( 'cd', function( error, stdout ) {
+		console.log( "`cd` stdout\t\t: " + stdout );
 	
-	
+		exec( 'set > envAfter.txt', doSecondCd );
+	} );
+}
+
+function doSecondCd() {
 	exec( 'cd', { cwd: process.cwd() }, function( error, stdout ) {
 		console.log( "`cd` stdout with cwd\t: " + stdout );
 	} );
-} );
+};
